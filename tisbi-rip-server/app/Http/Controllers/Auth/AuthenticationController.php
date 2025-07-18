79c0d3cf78b08a7581d0fe95d7197e3bfbfc89d2
Osato28 <osatonin@gmail.com>
2025-07-18 20:03:53 +0300

    Add authentication by login and by password

---
+++ tisbi-rip-server/app/Http/Controllers/Auth/AuthenticationController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Laravel\Sanctum\PersonalAccessToken;
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticationController extends Controller
+{
+    function loginByToken(Request $request)
+    {
+        try {
+            $tokenText = $request->input("token");
+            $token = PersonalAccessToken::findToken($tokenText);
+
+            if ($token == null) {
+                return response()->json(['success' => false, 'data' => [], 'error' => 'Token missing or expired, relogin via /api/user/login_by_pass to create a new token.'], 401);
+            }
+
+            /** @var User $user */
+            $user = $token->tokenable;
+
+            return response()->json(['success' => true, 'data' => [
+                'username' => $user->name,
+                'can_write' => $user->edit_privileges == 1 ? true : false
+            ], 'error' => '']);
+        } catch (\Throwable $e) {
+            return response()->json(['success' => false, 'data' => [], 'error' => $e->getMessage()], 504);
+        }
+    }
+
+    function loginByPass(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+        $authenticated = Auth::attempt($credentials);
+
+        if ($authenticated) {
+            $user = $request->user();
+            $token = $user->createToken('API')->plainTextToken;
+            return response()->json(['success' => true, 'data' => [
+                'username' => $user->name,
+                'token' => $token,
+                'can_write' => $user->edit_privileges == 1 ? true : false
+            ], 'error' => '']);
+        }
+
+        return response()->json(['success' => false, 'data' => [], 'error' => 'Failed to authorize.'], 401);
+    }
+}
--- tisbi-rip-server/routes/api.php
+++ tisbi-rip-server/routes/api.php
@@ -3,16 +3,23 @@
 use App\Http\Controllers\BonusController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\JobTitleController;
+use App\Http\Controllers\Auth\AuthenticationController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResources([
-    'employees' => EmployeeController::class,
-    'job_titles' => JobTitleController::class,
-    'bonuses' => BonusController::class
+Route::post('/user/login_by_token', [AuthenticationController::class, 'loginByToken']);
+Route::post('/user/login_by_pass', [AuthenticationController::class, 'loginByPass']);
+
+Route::apiResources(
+    [
+        'employees' => EmployeeController::class,
+        'job_titles' => JobTitleController::class,
+        'bonuses' => BonusController::class
     ]
 );
